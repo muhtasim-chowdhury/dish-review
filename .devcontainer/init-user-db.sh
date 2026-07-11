1f60bb623d02e866ee0c1773d47b188b09528f35
Muhtasim Chowdhury <muhtasimmc@gmail.com>
2026-07-11 00:29:30 +0000

    add another db user that django web application will use

---
+++ .devcontainer/init-user-db.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+set -e
+
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+	CREATE USER "$DJANGOUSER";
+	CREATE DATABASE "$DJANGODB";
+	GRANT ALL PRIVILEGES ON DATABASE docker TO docker;
+EOSQL
