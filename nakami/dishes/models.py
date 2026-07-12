cd886e38e783182f0369df0602dc832ee863ce16
Muhtasim Chowdhury <muhtasimmc@gmail.com>
2026-07-12 02:27:24 +0000

    add models

--- nakami/dishes/models.py
+++ nakami/dishes/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Restaurant(models.Model):
+    name = models.CharField(max_length=50)
+    date_added = models.DateTimeField("date added")
+
+
+class Dish(models.Model):
+    restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    price = models.IntegerField(default=0)
