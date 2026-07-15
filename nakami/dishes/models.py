2d2d841344c9c0ca039ad340563e55b1ea113646
Muhtasim Chowdhury <muhtasimmc@gmail.com>
2026-07-15 02:52:11 +0000

    add new field "address" to "Restaurant" model

--- nakami/dishes/models.py
+++ nakami/dishes/models.py
@@ -1,8 +1,10 @@
 from django.db import models
 from django.utils import timezone
+from datetime import timedelta
 
 class Restaurant(models.Model):
     name = models.CharField(max_length=50)
+    address = models.CharField(max_length=50, default="123 Main Street City, State")
     date_added = models.DateTimeField("date added")
 
     def __str__(self):
@@ -14,6 +16,10 @@ class Dish(models.Model):
     name = models.CharField(max_length=200)
     price = models.IntegerField(default=0)
     date_added = models.DateTimeField("date added", default=timezone.now)
+
+    def isNew(self):
+        diff = timezone.now() - self.date_added
+        return diff < timedelta(days=30)
 
     def __str__(self):
         return f"{self.name} from {self.restaurant}"
