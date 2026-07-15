fec0e9315e535d637c0b9b3daabf637b671dce7e
Muhtasim Chowdhury <muhtasimmc@gmail.com>
2026-07-15 02:08:49 +0000

    change to db model "Dish", new field "date_added"

--- nakami/dishes/models.py
+++ nakami/dishes/models.py
@@ -1,11 +1,19 @@
 from django.db import models
+from django.utils import timezone
 
 class Restaurant(models.Model):
     name = models.CharField(max_length=50)
     date_added = models.DateTimeField("date added")
+
+    def __str__(self):
+        return f"{self.name}"
 
 
 class Dish(models.Model):
     restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE)
     name = models.CharField(max_length=200)
     price = models.IntegerField(default=0)
+    date_added = models.DateTimeField("date added", default=timezone.now)
+
+    def __str__(self):
+        return f"{self.name} from {self.restaurant}"
